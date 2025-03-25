7dc4f01fe882c33c2160ab292901be637b6ac08b
Rahul Dey <rahuldey9623@gmail.com>
2025-03-25 16:20:51 +0530

    Update project references

--- ECommerce.ProductApi/ProductApi.Infrastructure/Repositories/ProductRepository.cs
+++ ECommerce.ProductApi/ProductApi.Infrastructure/Repositories/ProductRepository.cs
@@ -1,11 +1,14 @@
 using ECommerce.Shared.Repositories;
+using ECommerce.Shared.Services;
 using ProductApi.Domain.Entities;
 using ProductApi.Infrastructure.DB;
 using ProductApi.Infrastructure.Repositories.Interfaces;
 
 namespace ProductApi.Infrastructure.Repositories;
 
-public class ProductRepository(ProductDbContext dbContext) : BaseRepository<Product>(dbContext), IProductRepository
+public class ProductRepository(
+    ProductDbContext dbContext,
+    LoggerService logger) : BaseRepository<Product>(dbContext, logger), IProductRepository
 {
 
 }
