cf5a60faa31f7cd81a01b1c1213513cea58fa732
Rahul Dey <rahuldey9623@gmail.com>
2025-03-31 04:56:13 +0000

    Add http client config and retry strategy config

---
+++ ECommerce.OrderApi/OrderApi.Application/Extensions.cs
@@ -0,0 +1,52 @@
+using ECommerce.Shared.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Services;
+using OrderApi.Application.Services.Interfaces;
+using Polly;
+using Polly.Retry;
+
+namespace OrderApi.Application;
+
+public static class Extensions
+{
+    public static IServiceCollection AddApplicationService(
+        this IServiceCollection services,
+        IConfiguration configuration,
+        ILoggerService logger)
+    {
+        var baseAddress = configuration.GetSection("ApiGateway:BaseAddress").Value;
+
+        // Register http client service
+        services.AddHttpClient<IHttpService, HttpService>(option =>
+        {
+            option.BaseAddress = new Uri(baseAddress!);
+            option.Timeout = TimeSpan.FromSeconds(1);
+        });
+
+        // Create retry strategy
+        var retryStrategy = new RetryStrategyOptions()
+        {
+            ShouldHandle = new PredicateBuilder().Handle<TaskCanceledException>(),
+            BackoffType = DelayBackoffType.Constant,
+            UseJitter = true,
+            MaxRetryAttempts = 3,
+            Delay = TimeSpan.FromMicroseconds(500),
+            OnRetry = args =>
+            {
+                var message = $"OnRetry, Attempt: {args.AttemptNumber} Outcome: {args.Outcome}";
+                logger.LogError(message);
+                return ValueTask.CompletedTask;
+            }
+        };
+
+        services.AddResiliencePipeline("order-retry-pipeline", builder =>
+        {
+            builder.AddRetry(retryStrategy);
+        });
+
+        services.AddScoped<IOrderService, OrderService>();
+
+        return services;
+    }
+}
