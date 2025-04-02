988e73a0e97b6743de317dd71e73e70120baface
Rahul Dey <rahuldey9623@gmail.com>
2025-04-02 06:58:44 +0000

    configured service into program file

--- ECommerce.AuthApi/AuthApi/Program.cs
+++ ECommerce.AuthApi/AuthApi/Program.cs
@@ -1,7 +1,11 @@
+using AuthApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
+builder.Services.AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -11,6 +15,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseApiPolicy();
 app.UseHttpsRedirection();
 app.MapControllers();
 
